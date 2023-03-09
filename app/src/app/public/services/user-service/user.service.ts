922fdae11f33d4d220aa3a4c6456ebedb730513f
LTECHER <laurent.techer@ynov.com>
2023-03-09 22:07:10 +0100

    add public and private module to angular and registration form and angular material

---
+++ app/src/app/public/services/user-service/user.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators'
+import { UserInterface } from 'src/app/model/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient, private snackbar: MatSnackBar) { }
+
+  create(user: UserInterface): Observable<UserInterface> {
+    return this.http.post<UserInterface>('/api/users', user).pipe(
+    // return this.http.post<UserInterface>('http://localhost:3000/api/users', user).pipe(
+      tap((createdUser: UserInterface) => this.snackbar.open(`User ${createdUser.username} created successfully`, 'Close', {
+        duration: 2000, horizontalPosition: 'right', verticalPosition: 'top'
+      })),
+      catchError(e => {
+        this.snackbar.open(`User could not be created, due to: ${e.error.message}`, 'Close', {
+          duration: 5000, horizontalPosition: 'right', verticalPosition: 'top'
+        })
+        return throwError(e);
+      })
+    );
+  }
+
+}
